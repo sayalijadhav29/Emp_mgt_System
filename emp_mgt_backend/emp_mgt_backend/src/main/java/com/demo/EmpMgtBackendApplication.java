57fbbb13801193868195c55724cf73daeebf9977
sayalijadhav29 <sayalijadhav1916@gmail.com>
2026-06-17 11:59:28 +0530

    Initial commit - Emp_mgt_System

---
+++ emp_mgt_backend/emp_mgt_backend/src/main/java/com/demo/EmpMgtBackendApplication.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMgtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpMgtBackendApplication.class, args);
+	}
+}
